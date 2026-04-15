d58a573970021d41ca8d8ffa5e6ca2925aa5df49
sachinopboy2 <sachin25u7@gmail.com>
2026-04-15 13:59:15 +0530

    Update _inline.py

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -1,8 +1,6 @@
-Copyright (c) 2025 AnonymousX1025
-
-Licensed under the MIT License.
-
-This file is part of AnonXMusic
+#Copyright (c) 2025 AnonymousX1025
+#Licensed under the MIT License.
+#This file is part of AnonXMusic
 
 from pyrogram import types
 from pyrogram.enums import ButtonStyle
